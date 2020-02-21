0afb81d4ef71b22ca3540cb4d65557252655f176
Chan Woo Park <14cpark@gmail.com>
2020-02-21 11:20:59 -0800

    migrate malloc & free & merge functions

--- inp.c
+++ inp.c
@@ -10,6 +10,93 @@ void initializemem() {
 		memory[i] = 0;
 }
 
+void printmem() {
+	int i;
+	for(i = 0; i < 127; ++i) {
+		if (i%10 == 0)
+			printf("\n");
+		printf("%d: %i\t", i, memory[i]);
+		
+	}
+	printf("\n");
+}
+
+int getbestindex(int bytesize) {
+	int bestindex;
+	int minremainder = 999;
+	int index = 0;
+	while (index < 126) {
+		unsigned int payloadsz = (memory[index] >> 1) - 1; // size of payload at current index
+		unsigned int allocated = memory[index] & 1;
+		// printf("Payload: %d\n", payloadsz);
+		// printf("Index: %d\n", index);
+		// printf("Allocated: %d\n", allocated);
+		if (!allocated) {
+			if (payloadsz >= bytesize) {
+				int remainder = payloadsz - bytesize;
+				if (remainder < minremainder) {
+					bestindex = index;
+					minremainder = remainder;
+				}
+			}
+		}
+		index += (payloadsz + 1);
+	}
+	return bestindex;
+}
+
+void memmalloc(int bytenum) {
+	int bestindex = getbestindex(bytenum);
+	int header = ((bytenum + 1) << 1) | 1;
+	unsigned int payloadsz = (memory[bestindex] >> 1);
+	unsigned int remainder = payloadsz - (bytenum + 1);
+
+	// printf("Payloadsz: %i\n", payloadsz);
+	// printf("Remainder: %i\n", remainder);
+
+	memory[bestindex] = header;
+
+	unsigned int currval = memory[bestindex + bytenum + 1];
+
+	if (currval < remainder)
+		memory[bestindex + bytenum + 1] = (remainder << 1) | 0;
+
+	printf("%d\n", bestindex + 1);
+}
+	
+void memfree(int memnum) {
+	int index = memnum - 1;
+	int currval = memory[index] & 0b11111110;
+	// printf("%i\n", currval);
+	memory[index] = currval;
+	// printf("Index: %i\n", index);
+	// printf("Memory: %i\n", memory[index]);
+}
+
+void mergefreesectors() {
+	int index = 0;
+	while (index < 126) {
+		unsigned int payloadsz = (memory[index] >> 1)-1; // size of payload at current index
+		unsigned int allocated = memory[index] & 1;
+		if (!allocated) { // free block found
+			int nextindex = index + payloadsz + 1;
+			if (nextindex >= 127) // reached end of heap
+				break;
+			unsigned int nextpayloadsz = (memory[nextindex] >> 1) - 1;
+			unsigned int nextallocated = memory[nextindex] & 1;
+			if (!nextallocated) { // next is lso free : perform merge
+				int newpayloadsize = payloadsz + nextpayloadsz + 2;
+				int newheader = (newpayloadsize << 1); // shift left
+				memory[index] = newheader;
+			}
+			else
+				index += (payloadsz + 1);
+		}
+		else
+			index += (payloadsz + 1);
+	}
+}
+
 void start() {
 	char buffer[100];
 	initializemem();
@@ -18,33 +105,31 @@ void start() {
 		printf("> ");
 		fgets(buffer, 100, stdin); // store user input
 		strtok(buffer, "\n"); // remote newline character at end of input
-		char* firstcmd = strtok(buffer, " \t");
+		char* cmd = strtok(buffer, " \t");
 		
-		if (strcmp(firstcmd, "quit") == 0) {
+		if (strcmp(cmd, "quit") == 0) {
 			break;
 		}
-		if (strcmp(firstcmd, "malloc") == 0) {
-			printf("malloc\n");
-			// cmd = strtok(NULL,  " ");
-			// if (cmd != NULL) {
-				// int bytenum = atoi(cmd);
-				// if (bytenum >= 1 && bytenum <= 126)
-					// memmalloc(bytenum);
-			// }
+		if (strcmp(cmd, "malloc") == 0) {
+			cmd = strtok(NULL,  " \t");
+			if (cmd != NULL) {
+				int bytenum = atoi(cmd);
+				if (bytenum >= 1 && bytenum <= 126)
+					memmalloc(bytenum);
+			}
 		}
-		else if (strcmp(firstcmd, "free") == 0) {
-			printf("free\n");
-			// cmd = strtok(NULL, " ");
-			// if (cmd != NULL) {
-				// int memnum = atoi(cmd);
-				// if (memnum >= 1 && memnum <= 127)
-					// memfree(memnum);
-			// }
+		else if (strcmp(cmd, "free") == 0) {
+			cmd = strtok(NULL, " ");
+			if (cmd != NULL) {
+				int memnum = atoi(cmd);
+				if (memnum >= 1 && memnum <= 127)
+					memfree(memnum);
+			}
 		}
-		else if (strcmp(firstcmd, "blocklist") == 0) {
+		else if (strcmp(cmd, "blocklist") == 0) {
 			// blocklist();
 		}
-		else if (strcmp(firstcmd, "writemem") == 0) {
+		else if (strcmp(cmd, "writemem") == 0) {
 			printf("writemem\n");
 			// cmd = strtok(NULL, " ");
 			// if (cmd != NULL) {
@@ -58,8 +143,8 @@ void start() {
 				// }
 			// }
 		}
-		else if (strcmp(firstcmd, "printmem") == 0) {
-			printf("printmem\n");
+		else if (strcmp(cmd, "printmem") == 0) {
+			printmem();
 			// cmd = strtok(NULL, " ");
 			// if (cmd != NULL) {
 				// int addr = atoi(cmd);
@@ -73,9 +158,8 @@ void start() {
 				// }
 			// }
 		}
-		else if (strcmp(firstcmd, "loaddungeon") == 0) {
-			char* filename = strtok(NULL, " \t");
-		}
+		
+        mergefreesectors();
 		fflush(stdin);
 	}
 }
